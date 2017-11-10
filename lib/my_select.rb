aec5a2a4a6578eb3aaf2070af5441a80953fa0f1
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-10 16:37:06 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+    newArr = []
+    i = 0
+
+    while i < array.length
+      yield array[i]
+      if array == languages
+        newArr.push(array[i].upcase)
+      else
+        newArr.push(array[i].split(' ').first)
+      end
+      i += 1
+    end
+
+    newArr
 end
